26ec1682b7eecac8de95b36ee7206125ccb968f2
EdouardY <yuedouard@gmail.com>
2024-06-11 14:24:48 +0200

    init

--- src/pages/layout/PublicLayout.tsx
+++ src/pages/layout/PublicLayout.tsx
@@ -1,12 +1,10 @@
 import { FunctionComponent } from "react";
 import { Outlet } from "react-router-dom";
-import AuthenticationHeader from "@/components/layout/header/AuthenticationHeader";
 
 const PublicLayout: FunctionComponent = () => {
   return (
     <>
-      <AuthenticationHeader />
-      <main style={{ paddingTop: 70 }}>
+      <main>
         <Outlet />
       </main>
     </>
