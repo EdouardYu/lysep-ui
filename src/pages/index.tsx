daaae639399d0d297d345cff4f2004bda10875a3
EdouardY <yuedouard@gmail.com>
2024-06-12 02:24:43 +0200

    merge

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,4 +8,5 @@ export { default as Profile } from "@/pages/profile/Profile";
 export { default as Calender } from "@/pages/calender/Calender";
 export { default as Notifications } from "@/pages/notifications/Notifications";
 export { default as Alerts } from "@/pages/alerts/Alerts";
+export { default as Modules } from "@/pages/modules/Modules";
 export { default as PageNotFound } from "@/pages/page-not-found/PageNotFound";
--- src/routers/PrivateRouter.tsx
+++ src/routers/PrivateRouter.tsx
@@ -1,17 +1,27 @@
 import { FunctionComponent } from "react";
 import { Routes, Route } from "react-router-dom";
-import {PrivateLayout, Home, Profile, PageNotFound, Notifications, Calender, Alerts} from "@/pages";
+import {
+  PrivateLayout,
+  Profile,
+  PageNotFound,
+  Notifications,
+  Calender,
+  Alerts,
+  Modules,
+} from "@/pages";
 
 const PrivateRouter: FunctionComponent = () => {
   return (
     <Routes>
-        <Route element={<PrivateLayout />}>
-          <Route path="/calender" element={<Calender/>} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/notifications" element={<Notifications/>} />
-          <Route path="/alerts" element={<Alerts/>} />
-          <Route path="*" element={<PageNotFound />} />
-        </Route>
+      <Route element={<PrivateLayout />}>
+        <Route index element={<Calender />} />
+        <Route path="/calender" element={<Calender />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/notifications" element={<Notifications />} />
+        <Route path="/alerts" element={<Alerts />} />
+        <Route path="/modules" element={<Modules />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Route>
     </Routes>
   );
 };
